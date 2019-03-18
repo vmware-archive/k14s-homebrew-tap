9274d295c04c3b1c04125e88115292d244569f84
dmitriy kalinin <dkalinin@pivotal.io>
2019-03-18 14:17:34 -0700

    move to k14s org

--- ytt.rb
+++ ytt.rb
@@ -2,7 +2,7 @@ class Ytt < Formula
   desc "YAML Templating Tool"
   homepage "https://get-ytt.io"
   version "v0.1.0"
-  url "https://github.com/get-ytt/ytt/releases/download/v0.1.0/ytt-darwin-amd64"
+  url "https://github.com/k14s/ytt/releases/download/v0.1.0/ytt-darwin-amd64"
   sha256 "e771415781af72ca2dc8288f5a2d9bf617fe6a81fc3a95af6c67fa8352c0ca73"
 
   depends_on :arch => :x86_64
