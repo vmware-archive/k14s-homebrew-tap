80a74e3d4b6bdf3f7e7d1ae3a65bdbc25323d197
dmitriy kalinin <dkalinin@pivotal.io>
2019-03-18 15:35:57 -0700

    bump ytt to v0.2.0

--- ytt.rb
+++ ytt.rb
@@ -1,9 +1,9 @@
 class Ytt < Formula
   desc "YAML Templating Tool"
   homepage "https://get-ytt.io"
-  version "v0.1.0"
-  url "https://github.com/k14s/ytt/releases/download/v0.1.0/ytt-darwin-amd64"
-  sha256 "e771415781af72ca2dc8288f5a2d9bf617fe6a81fc3a95af6c67fa8352c0ca73"
+  version "v0.2.0"
+  url "https://github.com/k14s/ytt/releases/download/v0.2.0/ytt-darwin-amd64"
+  sha256 "2e49c7ef41a655b35ac192f40b9fd4c62cd05c177d6d24faecbdf054677d2c95"
 
   depends_on :arch => :x86_64
 
