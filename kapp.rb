f7f8a34db9e8b9830d9049eb938fd9ac771f1b29
Dmitriy Kalinin <dkalinin@pivotal.io>
2019-03-27 14:31:08 -0700

    Update kapp.rb

--- kapp.rb
+++ kapp.rb
@@ -1,9 +1,9 @@
 class Kapp < Formula
   desc "Kapp"
   homepage "https://get-kapp.io"
-  version "v0.3.0"
-  url "https://github.com/k14s/kapp/releases/download/v0.3.0/kapp-darwin-amd64"
-  sha256 "80a01662fa54d654f3f3c45db9538cd0d5aff318b4f4d7da4e949849dc66602e"
+  version "v0.4.0"
+  url "https://github.com/k14s/kapp/releases/download/v0.4.0/kapp-darwin-amd64"
+  sha256 "7a61cf9523281d94711c752ebd8d890068602f3fe6baf344895fbc59b9d5664e"
 
   depends_on :arch => :x86_64
 
