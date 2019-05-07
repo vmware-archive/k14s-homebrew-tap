5a2bdd8847a79aafc1d2ef7c723f80a095540d6e
Dmitriy Kalinin <dkalinin@pivotal.io>
2019-05-07 15:40:48 -0700

    Update ytt.rb

--- ytt.rb
+++ ytt.rb
@@ -1,9 +1,9 @@
 class Ytt < Formula
   desc "YAML Templating Tool"
   homepage "https://get-ytt.io"
-  version "v0.7.0"
-  url "https://github.com/k14s/ytt/releases/download/v0.7.0/ytt-darwin-amd64"
-  sha256 "aea8e18b8c634fa95007d8c2e10505cfeb17960fba3edf74ca5034010e1b2325"
+  version "v0.8.0"
+  url "https://github.com/k14s/ytt/releases/download/v0.8.0/ytt-darwin-amd64"
+  sha256 "15fc83ce80eac8d594495c768972492fc5b977cb71ee2d4429788e9e893d47ea"
 
   depends_on :arch => :x86_64
 
