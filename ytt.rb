cb47f642eb9f76259763835ce25eb6504deafeb1
Dmitriy Kalinin <cppforlife@gmail.com>
2019-08-29 11:56:50 -0700

    Update ytt.rb

--- ytt.rb
+++ ytt.rb
@@ -1,9 +1,9 @@
 class Ytt < Formula
   desc "YAML Templating Tool"
   homepage "https://get-ytt.io"
-  version "v0.19.0"
-  url "https://github.com/k14s/ytt/releases/download/v0.19.0/ytt-darwin-amd64"
-  sha256 "b64726d2ad5cb6edd31f4614f3e8bdb07bf6238466cd73a5104bc868afd04b70"
+  version "v0.20.0"
+  url "https://github.com/k14s/ytt/releases/download/v0.20.0/ytt-darwin-amd64"
+  sha256 "569191402672a42c5919feb1caad5a13fb41c01b2d849467211d32f66fd7d426"
 
   depends_on :arch => :x86_64
 
