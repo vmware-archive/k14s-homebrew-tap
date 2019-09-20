e368836c050a11e99f3c289ff2a5b68b77f569d4
Dmitriy Kalinin <cppforlife@gmail.com>
2019-09-20 12:43:35 -0700

    Update kapp.rb

--- kapp.rb
+++ kapp.rb
@@ -1,9 +1,9 @@
 class Kapp < Formula
   desc "Kapp"
   homepage "https://get-kapp.io"
-  version "v0.12.0"
-  url "https://github.com/k14s/kapp/releases/download/v0.12.0/kapp-darwin-amd64"
-  sha256 "2e9aec868607019d46fe2b8b1a9962d5fd347f7876fbe988df98c11a32d7efa7"
+  version "v0.13.0"
+  url "https://github.com/k14s/kapp/releases/download/v0.13.0/kapp-darwin-amd64"
+  sha256 "92edcf9de16ce17b95bcb85b83ed36cc11290efc02252d1029da7ce949aef730"
 
   depends_on :arch => :x86_64
 
