aae77ce30adc63bb87ae4f01e322fb0a8209b424
Dmitriy Kalinin <cppforlife@gmail.com>
2019-12-11 11:51:53 -0500

    Update kbld.rb

--- kbld.rb
+++ kbld.rb
@@ -1,9 +1,9 @@
 class Kbld < Formula
   desc "kbld helps working with container images used in Kubernetes configuration files"
   homepage "https://github.com/k14s/kbld"
-  version "v0.11.0"
-  url "https://github.com/k14s/kbld/releases/download/v0.11.0/kbld-darwin-amd64"
-  sha256 "f2e231877f86830e2324f189235c2788c54a865cc00b427694272d8967cd23ba"
+  version "v0.12.0"
+  url "https://github.com/k14s/kbld/releases/download/v0.12.0/kbld-darwin-amd64"
+  sha256 "2ca30f9e60c8fdcbec109fb73e61568e806c4c6620d9fd1ec0d0dd9b654aec50"
 
   depends_on :arch => :x86_64
 
