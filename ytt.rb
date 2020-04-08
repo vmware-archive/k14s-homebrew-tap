393bdc0b7eeb6d583ed95db2dcb81a88a8d93982
Dmitriy Kalinin <cppforlife@gmail.com>
2020-04-08 13:11:31 -0400

    Update ytt.rb

--- ytt.rb
+++ ytt.rb
@@ -1,14 +1,14 @@
 class Ytt < Formula
   desc "YAML Templating Tool"
   homepage "https://get-ytt.io"
-  version "v0.26.0"
+  version "v0.27.0"
 
   if OS.mac?
-    url "https://github.com/k14s/ytt/releases/download/v0.26.0/ytt-darwin-amd64"
-    sha256 "9275cd3033ec0276d0428cb73d4b5de87720491fc3261a1183a14d9344d8bfc6"
+    url "https://github.com/k14s/ytt/releases/download/v0.27.0/ytt-darwin-amd64"
+    sha256 "96cc4cd6131849964feebf0b82ed4302453af015a6b0edfb29a3af672ad6715d"
   elsif OS.linux?
-    url "https://github.com/k14s/ytt/releases/download/v0.26.0/ytt-linux-amd64"
-    sha256 "ea740f72a1825c00eabf83bfd6cb366adf4b2486992603d6850fac8487e92d8f"
+    url "https://github.com/k14s/ytt/releases/download/v0.27.0/ytt-linux-amd64"
+    sha256 "addd3f27dbffca09a8c7e7610e48dc53d127b08a91eb2b1097544327a6629a8c"
   end
 
   depends_on :arch => :x86_64
